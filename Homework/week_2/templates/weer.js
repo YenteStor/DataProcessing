556f38de0c68365a9fdc41ba5addc7b6fd1cd944
YenteStor <31654105+YenteStor@users.noreply.github.com>
2017-11-10 12:42:20 +0100

    Update weer.js

--- Homework/week_2/templates/weer.js
+++ Homework/week_2/templates/weer.js
@@ -1,1 +1,167 @@
+// Name: Yente Stor
+// Student number: 10676643
 
+// This script imports a weather dataset and implements it in a graph in weer.html
+// To run this file you need, weer.html and weer.py (to run the flask application)
+// run '$ python3 -m http.server 8080' when in a directory that holds weer.py and
+// a folder called templates that holds weer.html and weer.js
+// sources for canvas elements:
+// - https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+
+
+function getData(){
+    // function that converts a multiline text with dates and temperatures
+    // (YYYYMMDD,T) in a dictionary that has a key for an array of functional
+    // dates and a key with an array of numeric temperatures
+    // select raw data
+    rows = document.getElementById('rawdata').value.split("\n");
+
+    // dictionairy for functional data
+    var data = {
+        dates: [],
+        temps: []
+    };
+
+    // get functional data
+    for (row = 0; row < (rows.length - 1); row++){
+
+        // seperate date and temperature string
+        var row_string = rows[row].split(',');
+
+        // stores date string (YYYYMMDD) as functional date
+        data['dates'][row] = new Date(row_string[0].substring(0, 4),
+        (Number(row_string[0].substring(4, 6))-1), row_string[0].substring(6, 8));
+
+        // store temperature as number
+        data['temps'][row] = Number(row_string[1]);
+    };
+
+    return data;
+}
+
+
+function createTransform(domain, range){
+	// domain is a two-element array of the data bounds [domain_min, domain_max]
+	// range is a two-element array of the screen bounds [range_min, range_max]
+	// this gives you two equations to solve:
+	// range_min = alpha * domain_min + beta
+	// range_max = alpha * domain_max + beta
+ 		// a solution would be:
+
+    var domain_min = domain[0];
+    var domain_max = domain[1];
+    var range_min = range[0];
+    var range_max = range[1];
+
+    // formulas to calculate the alpha and the beta
+   	var alpha = (range_max - range_min) / (domain_max - domain_min);
+    var beta = range_max - alpha * domain_max;
+
+    // returns the function for the linear transformation (y= a * x + b)
+    return function(x){
+      return alpha * x + beta;
+    };
+}
+
+// finds minimun and maximum value of numeric list or date list
+function getDomain(data_list){
+
+    // chech if input is numeric
+    if (typeof(data_list[0]) != 'number'){
+        console.log('sorry, that is not a number...');
+        return undefined
+    }
+
+    // save biggest number
+    var domain_min = data_list.reduce(function(a,b){
+        return Math.max(a,b)
+    });
+
+    // save smallest number
+    var domain_max = data_list.reduce(function(a,b){
+        return Math.min(a,b)
+    });
+
+    return [domain_min, domain_max];
+}
+
+function relativeDays(dates_array){
+
+    days_array = []
+    var ms_day = 86400000;
+    date_0 = dates_array[0].getTime() - ms_day;
+
+    // calculate day number relative to date_0
+    for(i = 0; i<dates_array.length; i++){
+        days_array[i] = Math.round((dates_array[i].getTime() - date_0)/ms_day);
+    }
+
+    return days_array
+}
+
+function drawGraph(x_array, y_array){
+    // x_array  must be an array of the X values
+    // y_array must be an array of the y values
+    // uses the createTransform() function to get the transformed values
+    // to fit into the graph
+
+    // get canvas and create 2d context
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(10, 10, 500, 400);
+
+    // initiate measurements
+    edge = 100;
+    bg_width = 500;
+    bg_height = 400;
+    bg_edge = 30;
+
+    // graph background
+    ctx.fillStyle = 'rgb(200, 200, 0)';
+    ctx.fillRect(edge, edge, bg_width, bg_height);
+    ctx.beginPath();
+
+
+
+    // get transformation functions for x and y axis
+    x_transform = createTransform(getDomain(x_array),[edge + bg_edge, edge + bg_width - bg_edge])
+    y_transform = createTransform(getDomain(y_array),[edge + bg_edge, edge + bg_height - bg_edge])
+
+    // y axis
+    ctx.moveTo(edge + bg_edge, edge + bg_edge);
+    ctx.lineTo(edge + bg_edge, edge + bg_height - bg_edge);
+    ctx.stroke();
+
+    // x axis
+    ctx.moveTo(edge + bg_edge, y_transform(0));
+    ctx.lineTo(edge + bg_width - bg_edge, y_transform(0));
+    ctx.stroke();
+
+    // axis branches
+    x_transform
+
+    // starting point of data graph
+    ctx.beginPath();
+    ctx.moveTo(x_transform(x_array[0]),y_transform(y_array[0]))
+
+    // draw line through other data points
+    for(k = 1; k < x_array.length; k++){
+        ctx.lineTo(x_transform(x_array[k]),y_transform(y_array[k]))
+    }
+    ctx.stroke();
+
+    // title
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.font = '16px calibri'
+    ctx.fillText('AVERAGE  TEMPERATURE  IN  DE  BILT  OF  YENTE  STOR\'S  FIRST  YEAR', edge + bg_edge, edge + Math.round(bg_edge/1.5));
+
+};
+
+function main(){
+
+    data = getData();
+    days_ar = relativeDays(data['dates']);
+    drawGraph(days_ar,data['temps']);
+}
+
+main()
