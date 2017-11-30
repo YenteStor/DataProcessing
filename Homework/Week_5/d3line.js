926336d514526dd18925a201f839148024a9b72c
[Yente Stor] <[yentestor@gmail.com]>
2017-12-01 00:40:18 +0100

    crosshair finished

--- Homework/Week_5/d3line.js
+++ Homework/Week_5/d3line.js
@@ -5,12 +5,12 @@
 
   Sources:
   - http://bl.ocks.org/d3noob/4414436
-  -
+  - https://gist.github.com/lamchau/405f2d69fb3c80ad724a
   - https://stackoverflow.com/questions/37774053/d3-function-to-parse-the-date-not-working
   */
 function startjs(){
   // set margins
-  var margin = {top: 20, right: 20, bottom: 30, left: 60},
+  var margin = {top: 20, right: 100, bottom: 30, left: 60},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
@@ -41,9 +41,9 @@ function startjs(){
 
     // get data
     data.forEach(function(d) {
-      d.mini = +(d.mini/10);
-      d.gem = +(d.gem/10);
-      d.maxi = +(d.maxi/10);
+      d.mini = +(d.mini / 10);
+      d.gem = +(d.gem / 10);
+      d.maxi = +(d.maxi / 10);
       d.date = parseDate(d.date)
     });
 
@@ -52,6 +52,7 @@ function startjs(){
      d3.min(data, function(d) { return d.mini; }),
      d3.max(data, function(d) { return d.maxi; })
     ]);
+    console.log()
 
     // add x xAxis
     svg.append("g")
@@ -92,18 +93,101 @@ function startjs(){
       .y(function(d) { return y(d.maxi); });
 
     svg.append("path")
-		.attr("class", "line")
-		.attr("d", min_line(data))
-    .style("stroke", "steelblue");
-	svg.append("path")
-		.attr("class", "line")
-		.attr("d", max_line(data))
-		.style("stroke", "orangeRed");
-	svg.append("path")
-    .attr("class", "line")
-	  .attr("d",gem_line(data))
-	  .style("stroke", "olive")
-	  .style("stroke-dasharray", "2");
+		  .attr("class", "line")
+		  .attr("d", min_line(data))
+      .style("stroke", "steelblue");
+	  svg.append("path")
+		  .attr("class", "line")
+		  .attr("d", max_line(data))
+      .style("stroke", "orangeRed");
+	  svg.append("path")
+      .attr("class", "line")
+      .attr("d",gem_line(data))
+      .style("stroke", "olive")
+      .style("stroke-dasharray", "2");
 
+
+    var crosshair = svg.append("g")
+      .attr("class", "line");
+
+    // create vertical line
+    crosshair.append("line")
+      .attr("id", "crosshairY")
+      .attr("class", "crosshair");
+
+    var yMin = y(d3.min(data, function(d) { return d.mini; }))
+    var yMax = y(d3.max(data, function(d) { return d.maxi; }))
+    var xMin = x(d3.min(data, function(d) { return d.date; }))
+    var xMax = x(d3.min(data, function(d) { return d.date; }))
+
+    var dateText = svg.append("text")
+      .attr("y", 10)
+      .style("text-decoration","underline")
+    var maxText = svg.append("text")
+      .attr("y", 25)
+    var meanText = svg.append("text")
+      .attr("y", 40)
+    var minText = svg.append("text")
+      .attr("y", 55)
+
+
+
+  var dateFormat = d3.time.format("%B %d");
+
+    // console.log(data[150].mini)
+    console.log(data.length)
+    svg.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .style("fill", "none")
+      .style("pointer-events","all")
+      .on("mouseover", function() {
+          crosshair.style("display", null);
+      })
+      .on("mouseout", function() {
+          crosshair.style("display", "none");
+          minText.text("");
+          maxText.text("");
+          meanText.text("");
+          dateText.text("");
+      })
+      .on("mousemove", function() {
+          var mouse = d3.mouse(this);
+          var x = mouse[0];
+          var y = mouse[1];
+          var data_point = Math.round((x/width) * data.length)
+
+          crosshair.select("#crosshairY")
+            .attr("x1", mouse[0])
+            .attr("y1",yMin)
+            .attr("x2", mouse[0])
+            .attr("y2",yMax);
+
+          minText.attr("x", x + 5)
+          minText.text(function() {
+              return "min: " + data[data_point].mini
+          });
+
+          meanText.attr("x", x + 5)
+          meanText.text(function(){
+            return "mean: " + data[data_point].gem
+          });
+
+          maxText.attr("x", x + 5)
+          maxText.text(function() {
+            return "max: "+ data[data_point].maxi
+          });
+
+          dateText.attr("x", x + 5)
+          dateText.text(function() {
+            console.log(data[data_point].date)
+            console.log(dateFormat(data[data_point].date))
+            return dateFormat(data[data_point].date)
+          });
+
+
+
+        })
   });
 };
